735149b62ed91b171e04b4d44eaf7c0af1b882ca
Orlando Karam <orlando@thekarams.net>
2014-02-18 21:23:43 -0800

    initial

---
+++ FlaskApplication/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+import pymongo
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    text = { 'content': 'Welcome to learnmongo !' } 
+    return render_template("home.html",
+        title = 'Welcome',
+        text = text)
+
+
+app.debug = True
+if __name__ == "__main__":
+    app.run()
