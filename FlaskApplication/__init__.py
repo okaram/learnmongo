1c153f4d70c9832a4ef7a0278c39560afb730f4a
Orlando Karam <orlando@thekarams.net>
2014-02-19 19:51:16 -0800

    find ?

--- FlaskApplication/__init__.py
+++ FlaskApplication/__init__.py
@@ -3,17 +3,25 @@ from flask import render_template
 
 import pymongo
 
+client=pymongo.MongoClient("mongodb://okaram:1qaz2wsx@widmore.mongohq.com:10010/okaram")
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    client=pymongo.MongoClient("mongodb://okaram:1qaz2wsx@widmore.mongohq.com:10010/okaram")
     text = { 'content':  client.okaram.test.find_one()['name'] } 
     return render_template("home.html",
         title = 'Welcome',
         text = text)
 
-
+@app.route('/')
+def find():
+    obj=client.okaram.test.find({'_id':request.args.get('id')});
+    dict={'content': obj}
+    return render_template("home.html",
+        title = 'Welcome',
+        text = dict)
+    
 app.debug = True
 if __name__ == "__main__":
     app.run()
